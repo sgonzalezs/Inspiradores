0280581662ec8cbf97d8089dda21980706ebe0a8
Santiago <tiagosuarez22@gmail.com>
2020-08-18 14:48:56 -0500

    Config device sentido escucha

--- public/js/movil-detect.js
+++ public/js/movil-detect.js
@@ -1,44 +1,4 @@
 $(document).ready(function(){
-    detectDevice();
+    
 });
 
-function detectDevice(){
-    var orientation=screen.orientation.type;
-    if(orientation=="portrait-primary"){
-        $("body section").css("display", "none");
-        $("body").css({
-            "background-size":"100% 100vh"
-        });
-        $(".device").css("display", "block");
-        $(".footer img").css({
-            "position":"absolute"
-        });
-    }
-    
-    $(window).on("orientationchange",function( event ){
-        var orientation2=screen.orientation.type;
-        if(orientation2=="portrait-primary"){
-            $("body section").css("display", "none");
-            $("body").css({
-                "background-size":"100% 100vh"
-            });
-            $(".device").css("display", "block");
-            $(".footer img").css({
-                "position":"absolute",
-            });
-        }
-        
-        if(orientation2=="landscape-primary")
-        {
-            $("body section").css("display", "block");
-            $(".device").css("display", "none");
-            $("body").css({
-                "background-size":"100% 130%"
-            });
-            $(".footer img").css({
-                "position": "relative",
-                "width": "220px"
-            })
-        }
-    });
-}
--- public/js/sentidos.js
+++ public/js/sentidos.js
@@ -137,3 +137,44 @@ function loadInspiradores(identity){
         }
     }
 }
+
+function detectDevice(){
+    var orientation=screen.orientation.type;
+    if(orientation=="portrait-primary"){
+        $("body section").css("display", "none");
+        $("body").css({
+            "background-size":"100% 100vh"
+        });
+        $(".device").css("display", "block");
+        $(".footer img").css({
+            "position":"absolute"
+        });
+    }
+    
+    $(window).on("orientationchange",function( event ){
+        var orientation2=screen.orientation.type;
+        if(orientation2=="portrait-primary"){
+            $("body section").css("display", "none");
+            $("body").css({
+                "background-size":"100% 100vh"
+            });
+            $(".device").css("display", "block");
+            $(".footer img").css({
+                "position":"absolute",
+            });
+        }
+        
+        if(orientation2=="landscape-primary")
+        {
+            $("body section").css("display", "block");
+            $(".device").css("display", "none");
+            $("body").css({
+                "background-size":"100% 130%"
+            });
+            $(".footer img").css({
+                "position": "relative",
+                "width": "220px"
+            })
+        }
+    });
+}
