4613cea05d2a9451369dd1aea2818398fd61dc22
Santiago <tiagosuarez22@gmail.com>
2020-08-18 12:37:23 -0500

    Config device on view map

--- public/js/mapa/mapa.js
+++ public/js/mapa/mapa.js
@@ -67,6 +67,9 @@ function detectDevice(){
             "background-size": "100% 100vh"
         });
         $(".device").css("display", "block");
+        $(".footer img").css({
+            "position":"absolute"
+        });
     }
     
     $(window).on("orientationchange",function( event ){
@@ -74,24 +77,27 @@ function detectDevice(){
         console.log(orientation2);
         if(orientation2=="portrait-primary"){
             $("body section").css("display", "none");
-            // $("body").css({
-            //     "background-image": "url('../images/fondo/Fondo.png')",
-            //     "background-size": "cover",
-            //     "background-repeat":"no-repeat",
-            //     "background-size": "100% 100%"
-            // });
+            $("body").css({
+                "background-image": "url('../images/fondo/Fondo.png')",
+                "background-size": "cover",
+                "background-repeat":"no-repeat",
+                "background-size": "100% 100%"
+            });
             $(".device").css("display", "block");
+            $(".footer img").css({
+                "position":"absolute"
+            });
         }
         
         if(orientation2=="landscape-primary")
         {
             $("body section").css("display", "block");
-            // $("body").css({
-            //     "background-image": "url('../images/avatar/fondo_2.png')",
-            //     "background-size": "cover",
-            //     "background-repeat":"no-repeat",
-            //     "background-size": "100% 800px"
-            // });
+            $("body").css({
+                "background-image": "url('../images/avatar/fondo_2.png')",
+                "background-size": "cover",
+                "background-repeat":"no-repeat",
+                "background-size": "100% 100%"
+            });
             $(".device").css("display", "none");
             $(".footer img").css({
                 "position": "relative",
